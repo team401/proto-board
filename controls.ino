012f87737a6f9c63d8c0d1dcd83ad1f1dd961b2a
Preston <childrep@vt.edu>
2021-11-02 19:14:59 -0400

    Switched to using Servo.h to control microseconds

--- controls.ino
+++ controls.ino
@@ -1,15 +1,19 @@
-int pots[4] = {A0, A1, A2, A3};
-int pwms[4] = {3, 5, 6, 9};
+#include <Servo.h>
+
+int n = 4;
+int pots[n] = {A0, A1, A2, A3};
+int pwms[n] = {3, 5, 6, 9};
+Servo outputs[n];
 
 void setup() {
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < n; i++) {
     pinMode(pots[i], INPUT);
-    pinMode(pwms[i], OUTPUT);
+    outputs[i].attach(pwms[i]);
   }
 }
 
 void loop() {
-  for (int i = 0; i < 4; i++) {
-    analogWrite(pwms[i], analogRead(pots[i]) / 4);
+  for (int i = 0; i < n; i++) {
+    controllers[i].writeMicroseconds(1500 + analogRead(pots[i]) / 2);
   }
 }
