87a90b9c41b95db7ffb8d340c9a62371d2aa5e98
Preston <childrep@vt.edu>
2021-11-03 19:21:32 -0400

    Added deadband function

--- controls.ino
+++ controls.ino
@@ -1,6 +1,11 @@
 #include <Servo.h>
 
 const int n = 4;
+const int minVal = 1500;
+const int maxVal = 2000;
+const int midVal = minVal + (maxVal - minVal) / 2;
+const int width = 25;
+
 int pots[] = {A0, A1, A2, A3};
 int pwms[] = {3, 5, 6, 9};
 Servo outputs[n];
@@ -12,8 +17,20 @@ void setup() {
   }
 }
 
+int deadband(int raw) {
+  if (abs(raw - midVal) < width) {
+    return 0;
+  } else {
+    if (raw < mid) {
+      return (mid / (mid - width)) * raw;
+    } else {
+      return (mid / (mid + width)) * raw;
+    }
+  }
+}
+
 void loop() {
   for (int i = 0; i < n; i++) {
-    outputs[i].writeMicroseconds(1500 + analogRead(pots[i]) / 2);
+    outputs[i].writeMicroseconds(deadband(minVal + analogRead(pots[i]) / 2));
   }
 }
